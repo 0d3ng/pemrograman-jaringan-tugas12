e851e50778e21124778536dcf794e7c29276e763
od3ng <noprianto@polinema.ac.id>
2019-11-13 09:07:23 +0700

    commit pertama

---
+++ src/pemrograman/jaringan/tugas12/PemrogramanJaringanTugas12.java
@@ -0,0 +1,27 @@
+/*
+ * pemrograman-jaringan-tugas12
+
+ * Copyright (c) 2019
+ * All rights reserved.
+ * Written by od3ng created on Nov 13, 2019 9:04:40 AM
+ * Blog    : sinaungoding.com
+ * Email   : lepengdados@gmail.com
+ * Github  : 0d3ng
+ * Hp      : 085878554150
+ */
+package pemrograman.jaringan.tugas12;
+
+/**
+ *
+ * @author od3ng
+ */
+public class PemrogramanJaringanTugas12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
